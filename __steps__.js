12364e19a41291241343300a15151563f8abe9df
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-05-30 01:34:46 +0600

    use cors middleware

---
+++ __steps__.js
@@ -0,0 +1,6 @@
+/*
+
+* create a post api on the server side
+*client side send data via post method
+
+*/ 
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());    // cors middleware
+const users = [
+    {id:1, name: 'Mahafuj', email: 'mahafuj@gmail.com'},
+    {id:2, name: 'Emon', email: 'emon@gmail.com'},
+    {id:3, name: 'Rakib', email: 'rakib@gmail.com'}
+]
+
+
 app.get('/', (req, res) => {
     res.send('Uaera Management server is running');
+})
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.post('/users', (req, res) => {
+    console.log('post req send');
+    res.send('post req send');
 })
 
 app.listen(port, () => {
